e15a8f2747b7c8cc917b7f2626ead46a3590fa53
GC2C_08_ヨシダ_ハルキ <k025g1002@g.neec.ac.jp>
2026-05-20 14:38:25 +0900

    PG3_01_01修正版

--- PG3_00_01/main.cpp
+++ PG3_00_01/main.cpp
@@ -2,7 +2,6 @@
 #include <Windows.h>
 using namespace std;
 
-
 template <typename T>
 T Min(T a, T b) {
     return (a < b) ? a : b;
@@ -12,16 +11,15 @@ T Min(T a, T b) {
 template <>
 char Min(char a, char b) {
     cout << "数字以外は代入できません" << endl;
-    return a;
+    return a; 
 }
 
 int main() {
     SetConsoleOutputCP(65001);
 
-
+   
     int i1 = 10, i2 = 20;
     cout << "int: Min(" << i1 << ", " << i2 << ") = " << Min(i1, i2) << endl;
-
 
     float f1 = 3.14f, f2 = 2.71f;
     cout << "float: Min(" << f1 << ", " << f2 << ") = " << Min(f1, f2) << endl;
@@ -29,9 +27,8 @@ int main() {
     double d1 = 1.234, d2 = 5.678;
     cout << "double: Min(" << d1 << ", " << d2 << ") = " << Min(d1, d2) << endl;
 
-
     char c1 = 'A', c2 = 'B';
-    Min(c1, c2);
+    cout << "char: Min(" << c1 << ", " << c2 << ") = " << Min(c1, c2) << endl;
 
     return 0;
 }
