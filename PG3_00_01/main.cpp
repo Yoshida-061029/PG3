612ba2cbc419b9d207afc28a45d44dcd2a025a35
GC2C_08_ヨシダ_ハルキ <k025g1002@g.neec.ac.jp>
2026-06-10 15:18:25 +0900

    01_02

--- PG3_00_01/main.cpp
+++ PG3_00_01/main.cpp
@@ -1,38 +1,40 @@
-#include<stdio.h>
-#include<Windows.h>
+#include <stdio.h>
+#include <Windows.h>
 
-int Power2(int n) {
-    if (n == 0) return 1;
-    return Power2(n - 1) * 2;
-}
-
-int RecursiveWage(int a) {
-    if (a == 1) {
+int RecursiveWage(int n) {
+    if (n <= 1) {
         return 100;
     }
-    return RecursiveWage(a - 1) + 50 * Power2(a - 2);
+    return RecursiveWage(n - 1) * 2 - 50;
+}
+
+int RecursiveTotal(int n) {
+    if (n <= 0) {
+        return 0;
+    }
+    return RecursiveTotal(n - 1) + RecursiveWage(n);
 }
 
 int main() {
     SetConsoleOutputCP(65001);
 
-    int baseWage = 1072;
+    const int baseWage = 1072;
 
-    printf(" 時間  | 一般賃金 | 再帰賃金\n");
-    printf(" ----- |----------|----------\n");
+    printf("   時間 |　 一般賃金  | 再帰賃金\n");
+    printf("  ------|-------------|------------\n");
 
     for (int h = 1; h <= 10; h++) {
-        int normalWage = baseWage * h;
-        int recursiveWage = RecursiveWage(h);
-        printf("%3d時間 |%6d円 |%6d円\n", h, normalWage, recursiveWage);
+        int normalTotal = baseWage * h;
+        int recursiveTotal = RecursiveTotal(h);
+        printf("%3d時間 |%10d円 |%10d円\n", h, normalTotal, recursiveTotal);
     }
 
-    int lastNormalWage = baseWage * 10;
-    int lastRecursiveWage = RecursiveWage(10);
-    if (lastNormalWage > lastRecursiveWage) {
+    int lastNormal = baseWage * 10;
+    int lastRecursive = RecursiveTotal(10);
+    if (lastNormal > lastRecursive) {
         printf("一般賃金の方が高い\n");
     }
-    else if (lastRecursiveWage > lastNormalWage) {
+    else if (lastRecursive > lastNormal) {
         printf("再帰賃金の方が高い\n");
     }
     else {
