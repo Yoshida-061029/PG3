ffa2e984d15bc33c32c735a4fcc5a91a55115d02
GC2C_08_ヨシダ_ハルキ <k025g1002@g.neec.ac.jp>
2026-08-03 16:42:48 +0900

    06_02

--- PG3_00_01/main.cpp
+++ PG3_00_01/main.cpp
@@ -10,11 +10,14 @@ void PrintThread(uint32_t num) {
 
 int main() {
 
+
 	thread t1(PrintThread, 1);
-	t1.join();
 	thread t2(PrintThread, 2);
-	t2.join();
 	thread t3(PrintThread, 3);
+
+	
+	t1.join();
+	t2.join();
 	t3.join();
 
 	return 0;
