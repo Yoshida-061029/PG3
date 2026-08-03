64ed12432548370c25a099a4da0b6f1dc6ba7e30
GC2C_08_ヨシダ_ハルキ <k025g1002@g.neec.ac.jp>
2026-08-03 23:16:00 +0900

    06_02

--- PG3_00_01/main.cpp
+++ PG3_00_01/main.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
 #include <thread>
+#include <mutex>
+#include <condition_variable>
 
 using namespace std;
 
+mutex mtx;
+condition_variable cv;
+uint32_t current_turn = 1;
+
 void PrintThread(uint32_t num) {
 
+	unique_lock<mutex> lock(mtx);
+	cv.wait(lock, [num] { return current_turn == num; });
+
 	cout << "thread" << num << endl;
+
+	current_turn++;
+	cv.notify_all();
 }
 
 int main() {
-
 
 	thread t1(PrintThread, 1);
 	thread t2(PrintThread, 2);
 	thread t3(PrintThread, 3);
 
-	
 	t1.join();
 	t2.join();
 	t3.join();
